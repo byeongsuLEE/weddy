798bdfe8fac48045765b890cee7f5e5ada07b259
이호영 <hoo4053@naver.com>
2024-10-16 17:43:22 +0900

    feat(css): 로고

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,16 @@
-import ***REMOVED*** useState ***REMOVED*** from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ***REMOVED*** BrowserRouter, Route, Routes ***REMOVED*** from 'react-router-dom'
 import './App.css'
+import Main from './pages/MainPage'
 
 function App() ***REMOVED***
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src=***REMOVED***viteLogo***REMOVED*** className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src=***REMOVED***reactLogo***REMOVED*** className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick=***REMOVED***() => setCount((count) => count + 1)***REMOVED***>
-          count is ***REMOVED***count***REMOVED***
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element=***REMOVED***<Main />***REMOVED*** />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 ***REMOVED***
---
+++ frontend/src/pages/MainPage.tsx
@@ -0,0 +1,8 @@
+const Main = () => ***REMOVED***
+  return (
+    <>
+    <div>이채연 바보바보</div>
+    </>
+  )
+***REMOVED***
+export default Main;
