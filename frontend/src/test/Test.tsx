d74c1b51e3eed66be9610426ecf6dcd971415bf7
이채연 <dldus0621@naver.com>
2024-10-17 16:44:21 +0900

    feat(nft): nft 민팅 기능 완료
    S11P31C203-52

--- frontend/src/test/Test.tsx
+++ frontend/src/test/Test.tsx
@@ -1,11 +1,16 @@
 import ***REMOVED*** useState ***REMOVED*** from "react";
 import ***REMOVED*** wallet ***REMOVED*** from "./metaMask";
 import ***REMOVED*** digitalsign ***REMOVED*** from "./digitalSign";
+import ***REMOVED*** makeNFT ***REMOVED*** from "./nftMinting";
+
 const Test = () => ***REMOVED***
   const ***REMOVED*** connectWallet ***REMOVED*** = wallet();
   const ***REMOVED*** signature ***REMOVED*** = digitalsign();
+  const ***REMOVED*** mintNFT ***REMOVED*** = makeNFT();
+
   const [ sign, setSign ] = useState<any>();
   const [ account, setAccount ] = useState<any>();
+  const [ mint, setMint ] = useState<any>();
   
   const handleConnect = async () => ***REMOVED***
     const data = await connectWallet();
@@ -17,6 +22,11 @@ const Test = () => ***REMOVED***
     setSign(data);
   ***REMOVED***
 
+  const handleMiniting = async () => ***REMOVED***
+    const data = await mintNFT();
+    setMint(data);
+  ***REMOVED***
+
   return (
     <>
     <button onClick=***REMOVED***handleConnect***REMOVED***>지갑연결</button>
@@ -24,6 +34,9 @@ const Test = () => ***REMOVED***
 
     <button onClick=***REMOVED***handleSignature***REMOVED***>전자서명 요청</button>
     <div>전자서명 값 : ***REMOVED***sign***REMOVED***</div>
+
+    <button onClick=***REMOVED***handleMiniting***REMOVED***>NFT Minting</button>
+    <div>mint : ***REMOVED***mint***REMOVED***</div>
     </>
   )
 ***REMOVED***
