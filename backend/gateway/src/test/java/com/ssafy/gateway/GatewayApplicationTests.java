d7984c13bea9e7e97f14efed2e210ecfa3df7afc
이병수 <evil55@naver.com>
2024-10-22 17:35:44 +0900

    init : "back 기초 세팅"

---
+++ backend/gateway/src/test/java/com/ssafy/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests ***REMOVED***
+
+	@Test
+	void contextLoads() ***REMOVED***
+	***REMOVED***
+
+***REMOVED***
---
+++ backend/user/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication ***REMOVED***
+
+    public static void main(String[] args) ***REMOVED***
+        SpringApplication.run(DemoApplication.class, args);
+    ***REMOVED***
+
+***REMOVED***
