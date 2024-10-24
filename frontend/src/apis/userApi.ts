a5261ff0f27f716d2e954f8ac4a8758d64984a05
이채연 <dldus0621@naver.com>
2024-10-24 09:58:44 +0900

    feat(myPage): 정보 수정 api 완료

--- frontend/src/apis/userApi.ts
+++ frontend/src/apis/userApi.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-import ***REMOVED*** EditUserInfo ***REMOVED*** from "./user.type";
+import ***REMOVED*** userInformation ***REMOVED*** from "./user.type";
 
 const BASE_URL = 'http://localhost:8080/api/users'
 
@@ -12,8 +12,29 @@ export const getToken = async (userId: string | null): Promise<void> => ***REMOV
   console.log(response.data);
 ***REMOVED***;
 
+//== 회원 정보 ==//
+export const getUserInfo = async (): Promise<userInformation> => ***REMOVED***
+  // const response = await axios(***REMOVED***
+  //   method: 'get',
+  //   url: `$***REMOVED***BASE_URL***REMOVED***/userinfo`,
+  //   headers: ***REMOVED***
+  //     Authorization: `Bearer `
+  //   ***REMOVED***
+  // ***REMOVED***);
+  // console.log(response.data);
+  // return response.data;
+  
+  //== dumy ==//
+  return ***REMOVED***
+    'name': '이병수',
+    'phone': '010-1234-5678',
+    'email' : 'lbs@naver.com',
+    'address' : '광주광역시 광산구 하남동 1234'
+  ***REMOVED***;
+***REMOVED***;
+
 //== 회원 정보 수정 ==//
-export const editInfomation = async (userInfo?: EditUserInfo): Promise<void> => ***REMOVED***
+export const editInfomation = async (userInfo?: userInformation): Promise<void> => ***REMOVED***
   const response = await axios(***REMOVED***
     method: 'patch',
     url: BASE_URL,
--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,13 +1,39 @@
 // import ***REMOVED*** connectCoupleCode, getCoupleCode ***REMOVED*** from "@/apis/coupleApi";
-// import ***REMOVED*** EditUserInfo ***REMOVED*** from "@/apis/user.type";
-// import ***REMOVED*** editInfomation ***REMOVED*** from "@/apis/userApi";
-// import ***REMOVED*** useState ***REMOVED*** from "react";
+import ***REMOVED*** editInfomation, getUserInfo ***REMOVED*** from "@/apis/userApi";
+import ***REMOVED*** userInformation ***REMOVED*** from "@/apis/user.type";
+import ***REMOVED*** useEffect, useState ***REMOVED*** from "react";
 // import ***REMOVED*** useQuery ***REMOVED*** from "react-query";
-
 import TodoButton from "@/common/TodoButton";
+import ***REMOVED*** useQuery ***REMOVED*** from "react-query";
+
 
 const Mypage = () => ***REMOVED***
-  // const [ userInfo, setUserInfo ] = useState<EditUserInfo>();
+  const [ userInfo, setUserInfo ] = useState<userInformation>(***REMOVED***
+    name: '',
+    phone: '',
+    email: '',
+    address: '',
+  ***REMOVED***);
+
+  //== 회원 정보 ==//
+  const ***REMOVED*** data: userData, isSuccess ***REMOVED*** = useQuery('getUserInfo', getUserInfo);
+
+  //== userdata 업데이트 후 userInfo 업데이트 ==//
+  useEffect(() => ***REMOVED***
+    if (isSuccess && userData) ***REMOVED***
+      setUserInfo(userData);
+    ***REMOVED***
+  ***REMOVED***, [isSuccess, userData]);
+
+  //== 회원 정보 수정 ==//
+  const handleUpdate = async () => ***REMOVED***
+    await editInfomation(userInfo);
+  ***REMOVED***;
+
+  //== 상태 업데이트 ==//
+  const updateUserInfo = (key: keyof userInformation, value: string) => ***REMOVED***
+    setUserInfo((prev) => ***REMOVED*** return ***REMOVED*** ...prev, [key]: value***REMOVED*** ***REMOVED***);
+  ***REMOVED***;
 
   // //== 커플 코드 조회 ==//
   // const ***REMOVED*** data: myCode ***REMOVED*** = useQuery('getCoupleCode', getCoupleCode);
@@ -17,12 +43,7 @@ const Mypage = () => ***REMOVED***
   //   await connectCoupleCode(code);
   // ***REMOVED***;
 
-  // //== 회원 정보 수정 ==//
-  // const handleUpdate = async () => ***REMOVED***
-  //   await editInfomation(userInfo);
-  // ***REMOVED***;
-
-
+  
 
   return (
     <div className="m-5 bg-white h-[630px] rounded-xl p-5">
@@ -43,14 +64,36 @@ const Mypage = () => ***REMOVED***
           <span className="my-2 text-gray-600">이름</span>
           <span className="my-3 text-gray-600">전화번호</span>
           <span className="my-3 text-gray-600">이메일</span>
+          <span className="my-3 text-gray-600">쥬소</span>
         </div>
         <div className="flex flex-col mt-10">
-          <input defaultValue=***REMOVED***'김싸피'***REMOVED*** className="my-2 p-2 w-[150px] border border-gray-400 rounded-lg h-[30px]" type="text" />
-          <input defaultValue=***REMOVED***'010-1234-5678'***REMOVED*** className="my-2 p-2 w-[150px] border border-gray-400 rounded-lg h-[30px]" type="text" />
-          <input defaultValue=***REMOVED***'ssafy@ssafy.com'***REMOVED*** className="my-2 border p-2 w-[150px] border-gray-400 rounded-lg h-[30px]" type="text" />
+          <input
+            defaultValue=***REMOVED***userInfo.name***REMOVED***
+            className="my-2 p-2 w-[150px] border border-gray-400 rounded-lg h-[30px]"
+            type="text"
+            onChange=***REMOVED***(e) => updateUserInfo('name', e.target.value)***REMOVED***
+          />
+          <input 
+            defaultValue=***REMOVED***userInfo.phone***REMOVED*** 
+            className="my-2 p-2 w-[150px] border border-gray-400 rounded-lg h-[30px]" 
+            type="text" 
+            onChange=***REMOVED***(e) => updateUserInfo('phone', e.target.value)***REMOVED***
+          />
+          <input 
+            defaultValue=***REMOVED***userInfo.email***REMOVED*** 
+            className="my-2 border p-2 w-[150px] border-gray-400 rounded-lg h-[30px]" 
+            type="text"
+            onChange=***REMOVED***(e) => updateUserInfo('email', e.target.value)***REMOVED***
+          />
+          <input 
+            defaultValue=***REMOVED***userInfo.address***REMOVED*** 
+            className="my-2 border p-2 w-[150px] border-gray-400 rounded-lg h-[30px]" 
+            type="text"
+            onChange=***REMOVED***(e) => updateUserInfo('address', e.target.value)***REMOVED***
+          />
         </div>
       </div>
-      <div className="text-end my-5">
+      <div className="text-end my-5" onClick=***REMOVED***handleUpdate***REMOVED***>
         <TodoButton title="수정하기" colorId=***REMOVED***1***REMOVED*** />
       </div>
       <div className="bg-main1 flex flex-col items-center p-5 h-[150px] w-[310px] mt-12 rounded-xl">
