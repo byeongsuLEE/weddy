c9bbf06810a9f75a5c04e07b0e2965900914bf93
이호영 <hoo4053@naver.com>
2024-10-25 10:11:49 +0900

    feat(css): 리뷰 페이지 평점 추가

---
+++ frontend/src/components/ReviewPage/RatingBox.tsx
@@ -0,0 +1,16 @@
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
+
+
+const RatingBox = () => ***REMOVED***
+  console.log()
+  return (
+    <>
+      <Stack spacing=***REMOVED***1***REMOVED***>
+        <Rating name="size-medium" defaultValue=***REMOVED***0***REMOVED*** />
+      </Stack>
+    </>
+  )
+***REMOVED***
+
+export default RatingBox;
--- frontend/src/pages/ReviewPage.tsx
+++ frontend/src/pages/ReviewPage.tsx
@@ -2,6 +2,7 @@ import ***REMOVED*** ReviewData ***REMOVED*** from "@/apis/product.type";
 import ***REMOVED*** submitReview ***REMOVED*** from "@/apis/productApi";
 import Separate from "@/common/Separate";
 import TodoButton from "@/common/TodoButton";
+import RatingBox from "@/components/ReviewPage/RatingBox";
 import ***REMOVED*** useState ***REMOVED*** from "react";
 import ***REMOVED*** useNavigate, useParams ***REMOVED*** from "react-router-dom";
 
@@ -28,27 +29,34 @@ const Review = () => ***REMOVED***
   ***REMOVED***;
 
   return (
-    <>
+    <div className="mb-24">
       <div className="bg-white flex flex-col h-[200px] justify-center m-5 rounded-2xl p-10">
         <span>업체명</span>
         <Separate />
         <span>상품명</span>
         <span>총금액</span>
       </div>
-      <div className="bg-white h-[450px] m-5 rounded-2xl p-10 mb-24">
+        <div className="bg-white h-[120px] rounded-2xl p-5 m-5 flex flex-col items-center mb-3">
+        <h1 className="mb-1">상품은 만족하셨나요?</h1>
+        <RatingBox />
+        <span className="text-gray-400 text-xs my-2">선택해주세요.</span>
+        </div>
+      <div className="bg-white h-[350px] m-5 rounded-2xl p-5">
+        <div className="flex flex-col items-center ">
         <h1>어떤 점이 좋았나요?</h1>
         <textarea
-          className="border border-gray-400 rounded-lg p-5 h-[300px] w-[280px] mt-5"
+          className="border border-gray-400 rounded-lg p-5 h-[250px] w-[280px] mt-5"
           name="review"
           id="review"
           onChange=***REMOVED***(e) => updateReviewData('content', e.target.value)***REMOVED***
         >
         </textarea>
-        <div className="flex justify-end mt-3" onClick=***REMOVED***handleSubmit***REMOVED***>
-          <TodoButton title="리뷰 등록" colorId=***REMOVED***1***REMOVED*** />
         </div>
       </div>
-    </>
+        <div className="flex justify-end mt-3 mx-5" onClick=***REMOVED***handleSubmit***REMOVED***>
+          <TodoButton title="리뷰 등록" colorId=***REMOVED***1***REMOVED*** />
+        </div>
+    </div>
   )
 ***REMOVED***
 
