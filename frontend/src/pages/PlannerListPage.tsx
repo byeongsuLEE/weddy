7138736ecda1e466f366f17019bda035490d9eb0
이호영 <hoo4053@naver.com>
2024-10-29 16:04:16 +0900

    merge

--- frontend/src/pages/PlannerListPage.tsx
+++ frontend/src/pages/PlannerListPage.tsx
@@ -5,7 +5,7 @@ import ***REMOVED*** useState ***REMOVED*** from 'react';
 import ***REMOVED*** useParams, useNavigate ***REMOVED*** from 'react-router-dom';
 
 const PlannerList = () => ***REMOVED***
-  const [, setSelectedValue] = useState<string>();
+  const [selectedValue, setSelectedValue] = useState<string>();
   const category = useParams().category ?? '';
   const renderCategory = ***REMOVED***
     'studio': '스튜디오',
@@ -13,16 +13,15 @@ const PlannerList = () => ***REMOVED***
     'makeup': '메이크업',
   ***REMOVED***[category];
 
-  const handleValueChange = (value: string) => ***REMOVED***
-    setSelectedValue(value);
-    console.log("선택된 값:", value); // 콘솔에 선택된 값 출력
-  ***REMOVED***;
-
   const navigate = useNavigate();
   const goPlanner =()=>***REMOVED***
     navigate('/planner')
   ***REMOVED***
   
+  const handleSelect = (value: string) => ***REMOVED***
+    setSelectedValue(value);
+    console.log("선택된 값:", value); // 콘솔에 선택된 값 출력
+  ***REMOVED***;
 
   const dummyList = [
     ***REMOVED***
@@ -47,12 +46,12 @@ const PlannerList = () => ***REMOVED***
     ***REMOVED***,
   ]
   return (
-    <RadioGroup onValueChange=***REMOVED***handleValueChange***REMOVED***>
+    <RadioGroup value=***REMOVED***selectedValue***REMOVED*** onValueChange=***REMOVED***handleSelect***REMOVED***>
       <h1 className="text-center mt-10">***REMOVED***renderCategory***REMOVED***</h1>
       <div className="flex flex-wrap justify-center gap-8 mt-5 mb-5">
         ***REMOVED***dummyList.map((dummy, index) => (
           <div key=***REMOVED***index***REMOVED***>
-            <RecommendBox src=***REMOVED***dummy.src***REMOVED*** name=***REMOVED***dummy.name***REMOVED*** price=***REMOVED***dummy.price***REMOVED*** />
+            <RecommendBox isSelected=***REMOVED***selectedValue === dummy.name***REMOVED*** onSelect=***REMOVED***handleSelect***REMOVED*** src=***REMOVED***dummy.src***REMOVED*** name=***REMOVED***dummy.name***REMOVED*** price=***REMOVED***dummy.price***REMOVED*** />
           </div>
         ))***REMOVED***
       </div>
--- frontend/src/pages/PlannerPage.tsx
+++ frontend/src/pages/PlannerPage.tsx
@@ -1,31 +1,63 @@
 import PlannerBox from "@/components/PlannerPage/PlannerBox";
+import GiftIcon from "@/icons/GiftIcon";
 
 
 const Planner = () => ***REMOVED***
+  const dummyList = [
+    ***REMOVED***
+      category: '스튜디오',
+      company: '포에버마인스튜디오',
+      price: 10000000,
+      content: '앨범1권(20P) + 기본 액자 1개'
+    ***REMOVED***,
+    ***REMOVED***
+      category: '드레스',
+      company: '루이즈 블랑',
+      price: 37000000,
+      content: '[촬영+본식] 드레스4벌'
+    ***REMOVED***,
+    ***REMOVED***
+      category: '메이크업',
+      company: '',
+      price: 0,
+      content: ''
+    ***REMOVED***,
+  ]
+
+  const totalPrice = dummyList.reduce((acc, dummy) => acc + dummy.price, 0);
+
 
   return (
     <div className="relative">
-    <div className="m-5 flex flex-col items-center">
-      <h1 className="my-3 text-main2">WEDDY 플래너</h1>
-      <span className="text-sm mb-5">WEDDY 플래너가 추천하는 상품</span>
-      <PlannerBox title="스튜디오" />
-      <PlannerBox title="드레스" />
-      <PlannerBox title="메이크업" />
-    </div>
-    <div className="flex justify-end mr-10 mt-20 mb-24">
-      <div className="flex flex-col mr-3">
-        <span className="my-1">스튜디오 업체명:</span>
-        <span className="my-1">드레스 업체명: </span>
-        <span className="my-1">매이크업 업체명: </span>
-        <span className="font-bold">총 가격: </span>
+      <div className="m-5 flex flex-col items-center">
+        <h1 className="my-3 text-main2">WEDDY 플래너</h1>
+        <div className="flex items-center">
+        <span className="text-sm">WEDDY 플래너가 추천하는 상품</span>
+          <GiftIcon/>
+        </div>
+        ***REMOVED***dummyList.map((dummy, index) => (
+          <PlannerBox key=***REMOVED***index***REMOVED*** title=***REMOVED***dummy.category***REMOVED*** company=***REMOVED***dummy.company***REMOVED*** price=***REMOVED***dummy.price***REMOVED*** content=***REMOVED***dummy.content***REMOVED*** />
+        ))***REMOVED***
+
       </div>
-      <div className="flex flex-col text-end">
-        <span className="my-1">100000원</span>
-        <span className="my-1">100000원</span>
-        <span className="my-1">100000원</span>
-        <span className="font-bold"> 100000원</span>
+      <div className="flex justify-end mr-10 mt-20 mb-24">
+        <div className="flex flex-col mr-3">
+          ***REMOVED***dummyList.map((dummy, index) => (
+            <span key=***REMOVED***index***REMOVED*** className="my-1">
+              ***REMOVED***dummy.company ? dummy.company : "상품 없음"***REMOVED***
+            </span>
+          ))***REMOVED***
+          <span className="font-bold mt-2">총 가격: </span>
+        </div>
+        <div className="flex flex-col text-end">
+          ***REMOVED***dummyList.map((dummy, index) => (
+            <span key=***REMOVED***index***REMOVED*** className="my-1">***REMOVED***dummy.price.toLocaleString()***REMOVED***원</span>
+          ))***REMOVED***
+          <span className="font-bold">
+            ***REMOVED***totalPrice.toLocaleString()***REMOVED***원
+          </span>
+        </div>
       </div>
-    </div>
     </div>
   )
 ***REMOVED***
