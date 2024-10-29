63d904d7ce5520206ecdd4131ac5f9646d7dfbe3
이채연 <dldus0621@naver.com>
2024-10-29 09:21:46 +0900

    feat(rank): 랭킹 api 수정

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,6 +1,9 @@
 import BestBox from "@/components/MainPage/BestBox";
 import ***REMOVED*** MainCarousel ***REMOVED*** from "../components/MainPage/MainCarousel";
 import ***REMOVED*** useNavigate ***REMOVED*** from "react-router-dom";
+// import ***REMOVED*** useQuery ***REMOVED*** from "react-query";
+// import ***REMOVED*** getRankedProducts ***REMOVED*** from "@/apis/productApi";
+// import ***REMOVED*** Product ***REMOVED*** from "@/apis/product.type";
 
 const Main = () => ***REMOVED***
   const navigate = useNavigate();
@@ -24,6 +27,9 @@ const Main = () => ***REMOVED***
     navigate('/board/detail');
   ***REMOVED***;
 
+  // //== 베스트 ==//
+  // const ***REMOVED*** data: getRankedProductList ***REMOVED*** = useQuery('getRankedProducts', getRankedProducts);
+
   return (
     <div className="flex flex-col items-center mb-24">
       <MainCarousel dummyData=***REMOVED***dummyMain***REMOVED*** />
@@ -33,6 +39,12 @@ const Main = () => ***REMOVED***
             <BestBox key=***REMOVED***index***REMOVED*** index=***REMOVED***index+1***REMOVED*** src=***REMOVED***src***REMOVED*** title="test" price=***REMOVED***10000***REMOVED*** />
           </div>
         ))***REMOVED***
+
+        ***REMOVED***/* ***REMOVED***getRankedProductList?.map((product: Product, index) => (
+          <Link to=***REMOVED***`/board/detail/$***REMOVED***product.id***REMOVED***`***REMOVED***>
+            <BestBox key=***REMOVED***product.id***REMOVED*** index=***REMOVED***index+1***REMOVED*** src=***REMOVED***product.productImage[0]***REMOVED*** title="test" price=***REMOVED***10000***REMOVED***/>
+          </Link>
+        ))***REMOVED*** */***REMOVED***
       </div>
     </div>
   );
