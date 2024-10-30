2048145221b03aaf3c89705eb3e0cd1768280d6c
이채연 <dldus0621@naver.com>
2024-10-30 15:07:27 +0900

    refactor: console 삭제

--- frontend/src/components/BoardDetailPage/BoardContent.tsx
+++ frontend/src/components/BoardDetailPage/BoardContent.tsx
@@ -7,24 +7,9 @@ interface BoardContentProp ***REMOVED***
 ***REMOVED***
 
 const BoardContent = ( ***REMOVED*** product ***REMOVED***: BoardContentProp) => ***REMOVED***
-  console.log(product);
   const price = product?.price.toString().replace(/\B(?=(\d***REMOVED***3***REMOVED***)+(?!\d))/g, ',');
+  
   return (
-    // <div className="mx-5 mb-24">
-    //   <div className="flex flex-col my-5">
-    //     <span className="text-gray-500 text-sm">조슈아벨브라이덜</span>
-    //     <span>서울특별시 강남구</span>
-    //     <span className="font-bold mt-2">[ 촬영 + 본식 ] 드레스 4벌</span>
-    //     <span className="font-bold text-xl mt-2">15,000,000 원</span>
-    //   </div>
-    //   <Separate />
-    //   <div className="flex flex-col mt-5">
-    //     <span className="font-bold text-lg">상품 기본 정보</span>
-    //     <span className="font-bold">상품구성</span>
-    //     <span className="font-bold">방문상담 소요시간</span>
-    //   </div>
-    // </div>
-
     <div className="mx-5 mb-24">
       <div className="flex flex-col my-5">
         <span className="text-gray-500 text-sm">***REMOVED***product?.vendorName***REMOVED***</span>
