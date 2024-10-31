12263d2461581982bf56691a51888bf5d97ac93b
최승호 <148317140+S-Choi-1997@users.noreply.github.com>
2024-10-31 17:58:06 +0900

    chore : APIResponse add

--- backend/user/src/main/java/com/example/user/controller/MainController.java
+++ backend/user/src/main/java/com/example/user/controller/MainController.java
@@ -7,19 +7,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController ***REMOVED***
 
-    @GetMapping("/")
+//    @GetMapping("/")
+////    @ResponseBody
+//    public String mainAPI()***REMOVED***
+//        return "index";
+//    ***REMOVED***
+//
+//    @GetMapping("/login")
+//    public String login()***REMOVED***
+//        return "login";
+//    ***REMOVED***
+//
+//    @GetMapping("/api/auth")
 //    @ResponseBody
-    public String mainAPI()***REMOVED***
-        return "index";
-    ***REMOVED***
-
-    @GetMapping("/login")
-    public String login()***REMOVED***
-        return "login";
-    ***REMOVED***
-    @GetMapping("/api/auth")
-    @ResponseBody
-    public String auth()***REMOVED***
-        return "auth";
-    ***REMOVED***
+//    public String auth()***REMOVED***
+//        return "auth";
+//    ***REMOVED***
 ***REMOVED***
---
+++ backend/user/src/main/java/com/example/user/dto/APIResponse.java
@@ -0,0 +1,35 @@
+package com.example.user.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class APIResponse<T> ***REMOVED***
+    private int status;  // HTTP 상태 코드
+    private T data;      // 실제 응답 데이터
+
+    public APIResponse(int status, T data) ***REMOVED***
+        this.status = status;
+        this.data = data;
+    ***REMOVED***
+    private APIResponse(Builder<T> builder) ***REMOVED***
+        this.status = builder.status;
+        this.data = builder.data;
+    ***REMOVED***
+
+    public static class Builder<T>***REMOVED***
+        private int status;
+        private T data;
+        public Builder<T> status(int status)***REMOVED***
+            this.status = status;
+            return this;
+        ***REMOVED***
+        public Builder<T> data(T data)***REMOVED***
+            this.data = data;
+            return this;
+        ***REMOVED***
+        public APIResponse<T> build()***REMOVED***
+            return new APIResponse<T>(this);
+        ***REMOVED***
+    ***REMOVED***
+***REMOVED***
