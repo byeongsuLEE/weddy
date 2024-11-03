11b53e6c86fb080b370abe6571fb7968226ac559
이병수 <evil55@naver.com>
2024-11-03 21:46:29 +0900

    feat : 카프카 test 계약 생성 producer 작성 완료

--- backend/user/src/main/java/com/example/user/contract/service/ContractServiceImpl.java
+++ backend/user/src/main/java/com/example/user/contract/service/ContractServiceImpl.java
@@ -4,11 +4,15 @@ package com.example.user.contract.service;
 import com.example.user.contract.constant.ContractStatus;
 import com.example.user.contract.domain.Contract;
 import com.example.user.contract.dto.request.CreateContractRequestDto;
+import com.example.user.contract.dto.response.ContractResponseDto;
 import com.example.user.contract.repository.ContractRepository;
 import com.example.global.util.exception.ContractNotFoundException;
 import com.example.user.entity.UserEntity;
 import com.example.global.util.response.ErrorCode;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,13 +23,29 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Transactional
 public class ContractServiceImpl implements ContractService ***REMOVED***
+    // 나중에 제거해야함
+    @Value(value = "$***REMOVED***producers.topic1.name***REMOVED***")
+    private String TOPIC_PAYMENT;
+
+    private final KafkaTemplate<String , List<CreateContractRequestDto>> kafkaTemplate;
+
+    public void occurPaymentEvent(List<CreateContractRequestDto> paidProducts) throws JsonProcessingException ***REMOVED***
+        kafkaTemplate.send(TOPIC_PAYMENT, paidProducts);
+    ***REMOVED***
+
+
 
     private final ContractRepository contractRepository;
+
+
+
+
     @Override
     public List<Contract> createContract(UserEntity user, List<CreateContractRequestDto> createContractRequestListDto) ***REMOVED***
 
         List<Contract> contractList = createContractEntity(user, createContractRequestListDto);
-         return  contractRepository.saveAll(contractList);
+        List<ContractResponseDto> contractResponseDtos = mapToCreateContractResponseDtoList(contractList);
+        return  contractRepository.saveAll(contractList);
     ***REMOVED***
 
     @Override
@@ -69,7 +89,30 @@ public class ContractServiceImpl implements ContractService ***REMOVED***
                         .user(user)
                         .build())
                 .collect(Collectors.toList());
+
         return contractList;
     ***REMOVED***
-
+    public  List<ContractResponseDto> mapToCreateContractResponseDtoList(List<Contract> contract) ***REMOVED***
+        //TODO: CreateContractResponseDto 매핑
+        return contract.stream()
+                .map((contract1) -> ContractResponseDto.builder()
+                        .id(contract1.getId())
+                        .title(contract1.getProduct().getProduct_name())
+                        .content(contract1.getProduct().getProduct_content())
+                        .type(contract1.getProduct().getType())
+                        .code(contract1.getUser().getCoupleCode())
+                        .userName(contract1.getUser().getName())
+                        .status(contract1.getStatus())
+                        .totalMount(contract1.getTotalMount())
+                        .downPayment(contract1.getDownPayment())
+                        .finalPayment(contract1.getFinalPayment())
+                        .companyName(contract1.getCompanyName())
+                        .additionalTerms(contract1.getAdditionalTerms())
+                        .startDate(contract1.getStartDate())
+                        .endDate(contract1.getEndDate())
+                        .product(contract1.getProduct())
+                        .userId(contract1.getUser().getId())
+                        .build())
+                .collect(Collectors.toList());
+    ***REMOVED***
 ***REMOVED***
