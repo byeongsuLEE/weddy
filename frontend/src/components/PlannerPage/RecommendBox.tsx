cf9afee2ab2574f5b8ca1e768222dc0f25352e9e
이호영 <hoo4053@naver.com>
2024-11-04 11:38:10 +0900

    feat(css): 플래너 페이지 수정 완료

--- frontend/src/components/PlannerPage/RecommendBox.tsx
+++ frontend/src/components/PlannerPage/RecommendBox.tsx
@@ -1,38 +1,31 @@
-import ***REMOVED*** RadioGroupItem ***REMOVED*** from "@/components/ui/radio-group";
+import ***REMOVED*** Checkbox ***REMOVED*** from "@/components/ui/checkbox";
 import ***REMOVED*** Link ***REMOVED*** from "react-router-dom";
 
 interface RecommendBoxProps ***REMOVED***
   src: string;
   name: string;
   price: number;
-  onSelect: (value: string) => void;
-  isSelected: boolean; // 선택 여부를 나타내는 prop 추가
+  isSelected: boolean;
+  onSelect: () => void;
 ***REMOVED***
 
-const RecommendBox = (***REMOVED*** src, name, price, onSelect, isSelected ***REMOVED***: RecommendBoxProps) => ***REMOVED***
+const RecommendBox = (***REMOVED*** src, name, price, isSelected, onSelect ***REMOVED***: RecommendBoxProps) => ***REMOVED***
   return (
     <div className="flex flex-col">
       <Link to='/board/detail'>
         <img className="w-[150px] h-[150px] rounded-xl" src=***REMOVED***src***REMOVED*** alt="image" />
       </Link>
       <div className="flex items-center justify-between">
+        <Checkbox
+          checked=***REMOVED***isSelected***REMOVED***
+          onCheckedChange=***REMOVED***onSelect***REMOVED***
+        />
         <Link to='/board/detail'>
-          <div className="flex flex-col">
+          <div className="flex flex-col text-end">
             <span className="text-gray-500 mt-2">***REMOVED***name***REMOVED***</span>
             <span>***REMOVED***price.toLocaleString()***REMOVED***원</span>
           </div>
         </Link>
-        <div className="mr-3">
-          <button
-            onClick=***REMOVED***() => onSelect(name)***REMOVED***
-            className=***REMOVED***`mt-2 $***REMOVED***isSelected ? 'text-main2' : 'text-gray-400'***REMOVED***`***REMOVED*** // 선택된 경우 색상 변경
-          >
-            <div className="flex items-center justify-center rounded-full h-[40px] w-[40px] bg-white ">
-            <span className="font-bold text-xs">WEDDY</span>
-            </div>
-          </button>
-          <RadioGroupItem value=***REMOVED***name***REMOVED*** id=***REMOVED***name***REMOVED*** className="hidden" />
-        </div>
       </div>
     </div>
   );
--- frontend/src/pages/PlannerListPage.tsx
+++ frontend/src/pages/PlannerListPage.tsx
@@ -1,11 +1,18 @@
 import TodoButton from "@/common/TodoButton";
 import RecommendBox from "@/components/PlannerPage/RecommendBox";
 import ***REMOVED*** RadioGroup ***REMOVED*** from "@/components/ui/radio-group";
-import ***REMOVED*** useState ***REMOVED*** from 'react';
-import ***REMOVED*** useParams, useNavigate ***REMOVED*** from 'react-router-dom';
+import ***REMOVED*** useState ***REMOVED*** from "react";
+import ***REMOVED*** useNavigate, useParams ***REMOVED*** from 'react-router-dom';
+
 
 const PlannerList = () => ***REMOVED***
-  const [selectedValue, setSelectedValue] = useState<string>();
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+
+  const handleCheckboxChange = (index: number) => ***REMOVED***
+    setSelectedIndex(index === selectedIndex ? null : index);
+    console.log(dummyList[index]);
+  ***REMOVED***;
+
   const category = useParams().category ?? '';
   const renderCategory = ***REMOVED***
     'studio': '스튜디오',
@@ -15,14 +22,10 @@ const PlannerList = () => ***REMOVED***
 
   const navigate = useNavigate();
 
-  const goPlanner =()=>***REMOVED***
+  const goPlanner = () => ***REMOVED***
     navigate('/planner')
   ***REMOVED***
-  
-  const handleSelect = (value: string) => ***REMOVED***
-    setSelectedValue(value);
-    console.log("선택된 값:", value);
-  ***REMOVED***;
+
 
   const dummyList = [
     ***REMOVED***
@@ -47,18 +50,19 @@ const PlannerList = () => ***REMOVED***
     ***REMOVED***,
   ]
   return (
-    <RadioGroup value=***REMOVED***selectedValue***REMOVED*** onValueChange=***REMOVED***handleSelect***REMOVED***>
+    <RadioGroup >
       <h1 className="text-center mt-10">***REMOVED***renderCategory***REMOVED***</h1>
       <div className="flex flex-wrap justify-center gap-8 mt-5 mb-5">
         ***REMOVED***dummyList.map((dummy, index) => (
           <div key=***REMOVED***index***REMOVED***>
-            <RecommendBox isSelected=***REMOVED***selectedValue === dummy.name***REMOVED*** onSelect=***REMOVED***handleSelect***REMOVED*** src=***REMOVED***dummy.src***REMOVED*** name=***REMOVED***dummy.name***REMOVED*** price=***REMOVED***dummy.price***REMOVED*** />
+            <RecommendBox isSelected=***REMOVED***selectedIndex === index***REMOVED***
+              onSelect=***REMOVED***() => handleCheckboxChange(index)***REMOVED*** src=***REMOVED***dummy.src***REMOVED*** name=***REMOVED***dummy.name***REMOVED*** price=***REMOVED***dummy.price***REMOVED*** />
           </div>
         ))***REMOVED***
       </div>
       <div onClick=***REMOVED***goPlanner***REMOVED*** className="flex justify-end mr-10 mb-20">
-      ***REMOVED***/* 상품담기 api 연결하고 planner로 navigate */***REMOVED***
-      <TodoButton title="상품 담기"/>
+        ***REMOVED***/* 상품담기 api 연결하고 planner로 navigate */***REMOVED***
+        <TodoButton title="상품 담기" />
       </div>
     </RadioGroup>
   )
