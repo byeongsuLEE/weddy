70cf3e449a955ffbc4ca7680e1344b0b03bb9450
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2024-11-05 11:43:29 +0900

    feat(scheduler): 스프링 스케쥴러 활성화

---
+++ backend/cacheScheduler/src/main/java/ssafy/cachescheduler/CacheSchedulerApplication.java
@@ -0,0 +1,15 @@
+package ssafy.cachescheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CacheSchedulerApplication ***REMOVED***
+
+	public static void main(String[] args) ***REMOVED***
+		SpringApplication.run(CacheSchedulerApplication.class, args);
+	***REMOVED***
+
+***REMOVED***
