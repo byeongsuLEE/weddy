f745a32c337fda43d686371f58f6aa2cccd1214e
이채연 <dldus0621@naver.com>
2024-11-05 10:53:06 +0900

    feat(login): navigate
    userInfo가 없는경우 정보등록 페이지로 navigate

--- frontend/src/pages/CallBack.tsx
+++ frontend/src/pages/CallBack.tsx
@@ -1,4 +1,5 @@
-import ***REMOVED*** getToken ***REMOVED*** from "@/api/userApi";
+import ***REMOVED*** getToken, getUserInfo ***REMOVED*** from "@/api/userApi";
+import ***REMOVED*** useEffect, useState ***REMOVED*** from "react";
 import ***REMOVED*** useQuery ***REMOVED*** from "react-query";
 import ***REMOVED*** useLocation, useNavigate ***REMOVED*** from "react-router-dom";
 
@@ -7,14 +8,32 @@ const CallBack = () => ***REMOVED***
   const params = new URLSearchParams(useLocation().search);
   const userId = params.get('id');
 
+  const [ userInfoEnabled, setUserInfoEnabled ] = useState(false);
+  
   //== 토큰 정보 ==//
   useQuery(
     ['getToken', userId],
     () => getToken(userId ?? undefined),
     ***REMOVED***
-      enabled: !!userId
+      enabled: !!userId,
+      onSuccess: () => ***REMOVED***
+        setUserInfoEnabled(true);
+      ***REMOVED***
     ***REMOVED***
   );
+
+  //== 회원 정보 ==//
+  const ***REMOVED*** data: userInfo ***REMOVED*** = useQuery('getUserInfo', getUserInfo, ***REMOVED***
+    enabled: userInfoEnabled,
+  ***REMOVED***);
+
+  useEffect(() => ***REMOVED***
+    if (userInfo?.dateofWedding) ***REMOVED***
+      navigate('/');
+    ***REMOVED*** else ***REMOVED***
+      navigate('/userInfo');
+    ***REMOVED***
+  ***REMOVED***, [userInfo]);
 
   return null;
 ***REMOVED***
