89a52fd3fbb7b4d7274c9401d4394cc14c54afb0
byeongsuLEE <75319125+byeongsuLEE@users.noreply.github.com>
2024-11-05 12:14:21 +0900

    add : corsampping 추가

--- backend/user/src/main/java/com/example/user/config/CorsMvcConfig.java
+++ backend/user/src/main/java/com/example/user/config/CorsMvcConfig.java
@@ -14,7 +14,7 @@ public class CorsMvcConfig implements WebMvcConfigurer ***REMOVED***
                 .allowedOrigins("http://localhost:5173", "http://localhost:5174")  // 두 출처를 한 번에 추가
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")         // 허용할 HTTP 메서드 명시
                 .allowedHeaders("*")                                               // 모든 헤더 허용
-                .exposedHeaders("Set-Cookie", "Authorization")                     // 노출할 헤더 설정
+                .exposedHeaders("Authorization", "x-xsrf-token", "Access-Control-Allow-Headers", "Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers")        // 노출할 헤더 설정
                 .allowCredentials(true)                                            // 쿠키 포함 허용
                 .maxAge(3600L);
     ***REMOVED***
