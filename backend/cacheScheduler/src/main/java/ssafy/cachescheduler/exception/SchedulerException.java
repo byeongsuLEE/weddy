9e6f200490d24986981eaed1965c8139e072f419
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2024-11-05 13:42:02 +0900

    feat(scheduler): 실패 시 재시도 추가, cron 환경변수로 변경

---
+++ backend/cacheScheduler/src/main/java/ssafy/cachescheduler/exception/SchedulerException.java
@@ -0,0 +1,7 @@
+package ssafy.cachescheduler.exception;
+
+public class SchedulerException extends RuntimeException***REMOVED***
+    public SchedulerException(Exception e) ***REMOVED***
+        super("스케줄러 실패 : " + e.getMessage());
+    ***REMOVED***
+***REMOVED***
--- backend/cacheScheduler/src/main/java/ssafy/cachescheduler/scheduler/CacheScheduler.java
+++ backend/cacheScheduler/src/main/java/ssafy/cachescheduler/scheduler/CacheScheduler.java
@@ -2,10 +2,13 @@ package ssafy.cachescheduler.scheduler;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import ssafy.cachescheduler.exception.SchedulerException;
 import ssafy.cachescheduler.mapper.ProductMapper;
 import ssafy.cachescheduler.mapper.ReviewMapper;
 import ssafy.cachescheduler.util.RedisUtil;
@@ -20,8 +23,16 @@ public class CacheScheduler ***REMOVED***
     private final ProductMapper productMapper;
     private final ReviewMapper reviewMapper;
 
+    /**
+     * 이 작업이 실패할 경우 최대 3번까지 재시도하며, 재시도 간격은 2초
+     */
+    @Retryable(
+            value = ***REMOVED*** SchedulerException.class ***REMOVED***,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000)  // 2초 지연 후 재시도
+    )
     @Async("taskExecutor")
-    @Scheduled(cron = "0 0 0/1 * * *")
+    @Scheduled(cron = "$***REMOVED***cron.expression***REMOVED***")
     public void productCachingScheduler() ***REMOVED***
         log.info("productCachingScheduler 실행");
         productMapper.getAllProducts()
@@ -29,8 +40,16 @@ public class CacheScheduler ***REMOVED***
         log.info("productCachingScheduler 종료");
     ***REMOVED***
 
+    /**
+     * 이 작업이 실패할 경우 최대 3번까지 재시도하며, 재시도 간격은 2초
+     */
+    @Retryable(
+            value = ***REMOVED*** SchedulerException.class ***REMOVED***,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000)  // 2초 지연 후 재시도
+    )
     @Async("taskExecutor")
-    @Scheduled(cron = "0 0 0/1 * * *")
+    @Scheduled(cron = "$***REMOVED***cron.expression***REMOVED***")
     public void productReviewCachingScheduler() ***REMOVED***
         log.info("productReviewCachingScheduler 실행");
         reviewMapper.getAllReviews()
