e0cdd7cd0a79a36841785f786b2cc7d85056940a
이호영 <hoo4053@naver.com>
2024-11-05 11:28:05 +0900

    feat(css): 정보 입력, 마이페이지 수정

--- frontend/src/icons/RingIcon.tsx
+++ frontend/src/icons/RingIcon.tsx
@@ -1,6 +1,6 @@
 const RingIcon = () =>***REMOVED***
   return (
-  <img className="w-[70px] h-[60px]" src="/icons/ring.png" alt="ring icon" />
+  <img className="w-[50px] h-[50px] mx-3" src="/icons/ring.png" alt="ring icon" />
   )
 ***REMOVED***
 
