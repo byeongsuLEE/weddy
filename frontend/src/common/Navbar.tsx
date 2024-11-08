61a254b2f7b4ff2b57770e8a28e1cb8ebba226b7
이호영 <hoo4053@naver.com>
2024-11-08 14:21:11 +0900

    feat(css):드레스 리스트 페이지

--- frontend/src/common/Navbar.tsx
+++ frontend/src/common/Navbar.tsx
@@ -48,6 +48,9 @@ const Navbar = () => ***REMOVED***
       ***REMOVED***location === "sketch" && (
         <div>3D 드레스 스케치</div>
       )***REMOVED***
+      ***REMOVED***location === "dress" && (
+        <div>3D 드레스 스케치</div>
+      )***REMOVED***
 
       ***REMOVED***location === "board" && locationDetail === "detail" && (
         <div>제품 상세</div>
