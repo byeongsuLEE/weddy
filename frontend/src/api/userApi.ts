889ba3bad8bbab44bc4f7be4d6724ea85977f043
byeongsuLEE <75319125+byeongsuLEE@users.noreply.github.com>
2024-11-11 12:00:38 +0900

    feat(푸시)  : 푸시알림 수정

--- frontend/src/api/userApi.ts
+++ frontend/src/api/userApi.ts
@@ -10,14 +10,14 @@ export const getToken = async (userId?: string): Promise<void> => ***REMOVED***
     url: `$***REMOVED***BASE_URL***REMOVED***/token/super`,
     params: ***REMOVED***
       id: userId,
-    ***REMOVED***
+    ***REMOVED***,
   ***REMOVED***);
 
   if (userId) ***REMOVED***
     sessionStorage.setItem("userId", userId);
     sessionStorage.setItem("token", response.data.accessToken);
     sessionStorage.setItem("refreshToken", response.data.refreshToken);
-  ***REMOVED***;
+  ***REMOVED***
 ***REMOVED***;
 
 //== 로그아웃 ==//
@@ -26,7 +26,7 @@ export const logout = () => ***REMOVED***
     method: "post",
     url: `$***REMOVED***BASE_URL***REMOVED***/logout`,
     headers: ***REMOVED***
-      Authorization: sessionStorage.getItem("token")
+      Authorization: sessionStorage.getItem("token"),
     ***REMOVED***,
   ***REMOVED***);
 ***REMOVED***;
@@ -37,7 +37,7 @@ export const getUserInfo = async (): Promise<userInformation[]> => ***REMOVED***
     method: "get",
     url: BASE_URL,
     headers: ***REMOVED***
-      Authorization: sessionStorage.getItem("token")
+      Authorization: sessionStorage.getItem("token"),
     ***REMOVED***,
   ***REMOVED***);
   console.log(response.data.data);
@@ -52,13 +52,15 @@ export const editProfile = async (file: FormData): Promise<void> => ***REMOVED**
     headers: ***REMOVED***
       Authorization: sessionStorage.getItem("token"),
     ***REMOVED***,
-    data: file
-    ***REMOVED***);
-    console.log(response.data);
-  ***REMOVED***;
+    data: file,
+  ***REMOVED***);
+  console.log(response.data);
+***REMOVED***;
 
 //== 회원 정보 수정 ==//
-export const editInformation = async ( userInfo?: userInformation ): Promise<void> => ***REMOVED***
+export const editInformation = async (
+  userInfo?: userInformation
+): Promise<void> => ***REMOVED***
   console.log(userInfo);
   // await axios(***REMOVED***
   //   method: "patch",
@@ -71,17 +73,20 @@ export const editInformation = async ( userInfo?: userInformation ): Promise<voi
 ***REMOVED***;
 
 //fcm 토큰 저장
-export const saveFcmToken = async (fcmToken: string, userId:string): Promise<void> => ***REMOVED***
+export const saveFcmToken = async (
+  fcmToken: string,
+  userId: string
+): Promise<void> => ***REMOVED***
   await axios(***REMOVED***
     method: "patch",
     url: `$***REMOVED***BASE_URL***REMOVED***/fcm-token/$***REMOVED***userId***REMOVED***`,
     headers: ***REMOVED***
       Authorization: sessionStorage.getItem("token"),
-      'Content-Type': 'text/plain',
+      "Content-Type": "application/json",
     ***REMOVED***,
-    data: fcmToken 
+    data: ***REMOVED*** fcmToken: fcmToken ***REMOVED***,
   ***REMOVED***);
-***REMOVED***
+***REMOVED***;
 
 // 커플코드로 fcm 토큰 조회
 export const getFcmToken = async (coupleCode: string): Promise<string> => ***REMOVED***
@@ -93,4 +98,4 @@ export const getFcmToken = async (coupleCode: string): Promise<string> => ***REM
     ***REMOVED***,
   ***REMOVED***);
   return response.data.data;
-***REMOVED***
+***REMOVED***;
