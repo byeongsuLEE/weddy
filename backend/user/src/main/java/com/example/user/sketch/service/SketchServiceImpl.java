dcdcdd3d9612aa2b14f8317ad42eb7db82ed91d8
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2024-11-12 17:06:07 +0900

    feat(sketch): 사용자 스케치 등록, 조회

---
+++ backend/user/src/main/java/com/example/user/sketch/service/SketchServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.user.sketch.service;
+
+import com.example.user.common.service.GCSImageService;
+import com.example.user.sketch.dto.request.SketchRequestDto;
+import com.example.user.sketch.dto.response.SketchResponseDto;
+import com.example.user.sketch.entity.Sketch;
+import com.example.user.sketch.repository.SketchRepository;
+import com.example.user.user.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SketchServiceImpl implements SketchService ***REMOVED***
+    private final SketchRepository sketchRepository;
+    private final GCSImageService gcsImageService;
+
+    @Override
+    @Transactional
+    public SketchResponseDto registerSketch(final SketchRequestDto sketchRequestDto, final MultipartFile image, final UserEntity user) ***REMOVED***
+        if(image != null && !image.isEmpty())***REMOVED***
+            String imageUrl = gcsImageService.uploadImage(image);
+            Sketch sketch = sketchRepository.save(Sketch.builder().requestDto(sketchRequestDto).image(imageUrl).user(user).build());
+            return sketch.getUserSketch(sketch);
+        ***REMOVED***
+        throw new IllegalArgumentException("이미지 업로드 실패");
+    ***REMOVED***
+
+    @Override
+    public List<SketchResponseDto> getAllSketchesByUser(UserEntity user) ***REMOVED***
+        return sketchRepository.findByUser(user)
+                .stream()
+                .map(sketch -> sketch.getUserSketch(sketch))
+                .toList();
+    ***REMOVED***
+***REMOVED***
