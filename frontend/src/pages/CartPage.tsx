51dff8bb9372c9487274d3dfb3ad39039a0dbf8c
이채연 <dldus0621@naver.com>
2024-11-12 10:11:33 +0900

    feat(cart): mutation 수정

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -4,30 +4,29 @@ import ***REMOVED*** deleteFromCart, getCartItems ***REMOVED*** from "@/api/prod
 import TodoButton from "@/common/TodoButton";
 import CartListBox from "@/components/CartPage/CartListBox";
 import ***REMOVED*** useState ***REMOVED*** from "react";
-import ***REMOVED*** QueryClient, useMutation, useQuery ***REMOVED*** from "react-query";
+import ***REMOVED*** useMutation, useQuery, useQueryClient ***REMOVED*** from "react-query";
 import ***REMOVED*** useNavigate ***REMOVED*** from "react-router-dom";
 
 const CartPage = () => ***REMOVED***
   const navigate = useNavigate();
-  const queryClient = new QueryClient();
-
-  const [selectedList, setSelectedList] = useState<***REMOVED*** [type: string]: Product | null ***REMOVED***>(***REMOVED***
+  const queryClient = useQueryClient();
+  const ***REMOVED*** data: cartList ***REMOVED*** = useQuery("getCartItems", getCartItems);
+  
+  const [selectedList, setSelectedList] = useState<***REMOVED*** [type: string]: Product | null; ***REMOVED***>(***REMOVED***
     STUDIO: null,
     DRESS: null,
     MAKEUP: null,
   ***REMOVED***);
 
-  const ***REMOVED*** data: cartList ***REMOVED*** = useQuery("getCartItems", getCartItems);
-
   const deleteMutation = useMutation(deleteFromCart, ***REMOVED***
     onSuccess: () => ***REMOVED***
       queryClient.invalidateQueries("getCartItems");
     ***REMOVED***
-  ***REMOVED***)
-  
-  const handleRemoveItem = (productId: string) => ***REMOVED***
+  ***REMOVED***);
+
+  const handleRemove = (productId: string) => ***REMOVED***
     deleteMutation.mutate(productId);
-  ***REMOVED***
+  ***REMOVED***;
 
   //== 총 가격 계산 ==//
   const totalAmount = Object.values(selectedList).reduce((acc, item) => acc + (Number(item?.price) || 0), 0).toLocaleString();
@@ -50,44 +49,47 @@ const CartPage = () => ***REMOVED***
   return (
     <div className="flex flex-col relative">
       <div className="m-5 flex flex-col items-center">
-
-        ***REMOVED***['STUDIO', 'DRESS', 'MAKEUP'].map((category: string) => (
+        ***REMOVED***["STUDIO", "DRESS", "MAKEUP"].map((category: string) => (
           <CartListBox
-          key=***REMOVED***category***REMOVED***
-          category=***REMOVED***category***REMOVED***
-          productList=***REMOVED***cartList?.filter((item: Product) => item.type === category)***REMOVED***
-          selectedList=***REMOVED***selectedList***REMOVED***
-          onProductChange=***REMOVED***handleProductChange***REMOVED***
-          onRemove=***REMOVED***handleRemoveItem***REMOVED***
+            key=***REMOVED***category***REMOVED***
+            category=***REMOVED***category***REMOVED***
+            productList=***REMOVED***cartList?.filter((item: Product) => item.type === category)***REMOVED***
+            selectedList=***REMOVED***selectedList***REMOVED***
+            onProductChange=***REMOVED***handleProductChange***REMOVED***
+            onRemove=***REMOVED***handleRemove***REMOVED***
           />
         ))***REMOVED***
-
       </div>
-      
+
       <div className="flex justify-end mr-10 mt-14">
         <div className="flex flex-col mr-3">
-        ***REMOVED***Object.entries(selectedList).map(([category, item]) =>
-          item?.name ? (
-            <span key=***REMOVED***category***REMOVED*** className="my-1">
-              ***REMOVED***item.name***REMOVED***
-            </span>
-          ) : null
-        )***REMOVED***
+          ***REMOVED***Object.entries(selectedList).map(([category, item]) =>
+            item?.name ? (
+              <span key=***REMOVED***category***REMOVED*** className="my-1">
+                ***REMOVED***item.name***REMOVED***
+              </span>
+            ) : null
+          )***REMOVED***
           <span className="font-bold mt-2">총 가격: </span>
         </div>
         <div className="flex flex-col text-end">
-        ***REMOVED***Object.entries(selectedList).map(([category, item]) =>
-          item?.price ? (
-            <span key=***REMOVED***category***REMOVED*** className="my-1">
-              ***REMOVED***Number(item.price).toLocaleString()***REMOVED***원
-            </span>
-          ) : null
-        )***REMOVED***
-          <span className="font-bold mt-2">***REMOVED***totalAmount.toLocaleString()***REMOVED***원</span>
+          ***REMOVED***Object.entries(selectedList).map(([category, item]) =>
+            item?.price ? (
+              <span key=***REMOVED***category***REMOVED*** className="my-1">
+                ***REMOVED***Number(item.price).toLocaleString()***REMOVED***원
+              </span>
+            ) : null
+          )***REMOVED***
+          <span className="font-bold mt-2">
+            ***REMOVED***totalAmount.toLocaleString()***REMOVED***원
+          </span>
         </div>
       </div>
-      
-      <div className="flex justify-end mr-10 mt-5 mb-24" onClick=***REMOVED***handleCreateContract***REMOVED***>
+
+      <div
+        className="flex justify-end mr-10 mt-5 mb-24"
+        onClick=***REMOVED***handleCreateContract***REMOVED***
+      >
         <TodoButton title="계약 요청" />
       </div>
     </div>
