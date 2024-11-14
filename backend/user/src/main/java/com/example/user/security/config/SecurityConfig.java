e6e5a4d6947c0409c6fdf5702693c28f3c94028e
이병수 <75319125+byeongsuLEE@users.noreply.github.com>
2024-11-14 15:53:01 +0900

    fix : security conifg 수정

--- backend/user/src/main/java/com/example/user/security/config/SecurityConfig.java
+++ backend/user/src/main/java/com/example/user/security/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig ***REMOVED***
             @Override
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) ***REMOVED***
                 CorsConfiguration configuration = new CorsConfiguration();
-                configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+                configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174","https://weddy.info"));
                 configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
                 configuration.setAllowedHeaders(List.of("*"));
                 configuration.setAllowCredentials(true);
@@ -54,6 +54,8 @@ public class SecurityConfig ***REMOVED***
         http.authorizeHttpRequests((auth) -> auth
                 .requestMatchers(
                         "/",
+                        "/oauth2",
+                        "/api/oauth2",
                         "/login",
                         "/login/**",
                         "/users/token",
