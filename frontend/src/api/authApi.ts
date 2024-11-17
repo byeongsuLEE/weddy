4a9fbbe136c0a9a751c1bfb78205b094fa1edcb2
이호영 <hoo4053@naver.com>
2024-11-17 15:18:27 +0900

    fix: 로그인 수정

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -1,5 +1,3 @@
-import ***REMOVED*** useNavigate ***REMOVED*** from "react-router-dom";
-
 const BASE_URL = "https://weddy.info/oauth2/authorization";
 //== 네이버 로그인 ==//
 export const naverLogin = () => ***REMOVED***
@@ -8,7 +6,5 @@ export const naverLogin = () => ***REMOVED***
 
 //== 구글 로그인 ==//
 export const googleLogin = () => ***REMOVED***
-  const navigate = useNavigate();
   // window.location.href = `$***REMOVED***BASE_URL***REMOVED***/google`;
-  navigate('/callback?id=3');
 ***REMOVED***;
