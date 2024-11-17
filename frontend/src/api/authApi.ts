2ff9e1329617729472439578efc2ac7ab2565877
이채연 <dldus0621@naver.com>
2024-11-17 14:13:47 +0900

    fix(api)

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "https://weddy.info/oauth2/authorization";
+const BASE_URL = "https://weddy.info/api/oauth2/authorization";
 //== 네이버 로그인 ==//
 export const naverLogin = () => ***REMOVED***
   window.location.href = `$***REMOVED***BASE_URL***REMOVED***/naver`;
