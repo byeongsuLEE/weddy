e28db86aabadab74d268f8881029ba5275198ba8
이채연 <dldus0621@naver.com>
2024-11-17 14:53:30 +0900

    fix

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -1,3 +1,5 @@
+import ***REMOVED*** useNavigate ***REMOVED*** from "react-router-dom";
+
 const BASE_URL = "https://weddy.info/oauth2/authorization";
 //== 네이버 로그인 ==//
 export const naverLogin = () => ***REMOVED***
@@ -6,5 +8,9 @@ export const naverLogin = () => ***REMOVED***
 
 //== 구글 로그인 ==//
 export const googleLogin = () => ***REMOVED***
-  window.location.href = `$***REMOVED***BASE_URL***REMOVED***/google`;
+  const navigate = useNavigate();
+  // window.location.href = `$***REMOVED***BASE_URL***REMOVED***/google`;
+  sessionStorage.setItem('token', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywidXNlck5hbWUiOiLstZzsirntmLgiLCJjb3VwbGVDb2RlIjoiamM3VllhIiwiaWF0IjoxNzMxNDgwNjEwLCJleHAiOjE3MzQwNzI2MTB9.Cyd6ujpcIBHibkdfBBq-OApOHykmVdlzRnRfyp5rfXI')
+  sessionStorage.setItem('userId', '3')
+  navigate('/callback');
 ***REMOVED***;
