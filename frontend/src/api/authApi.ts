3dff98c1a5e6d1bd9aea187899705414d2fbade0
이채연 <dldus0621@naver.com>
2024-11-17 15:06:25 +0900

    fix

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -10,7 +10,5 @@ export const naverLogin = () => ***REMOVED***
 export const googleLogin = () => ***REMOVED***
   const navigate = useNavigate();
   // window.location.href = `$***REMOVED***BASE_URL***REMOVED***/google`;
-  sessionStorage.setItem('token', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywidXNlck5hbWUiOiLstZzsirntmLgiLCJjb3VwbGVDb2RlIjoiamM3VllhIiwiaWF0IjoxNzMxNDgwNjEwLCJleHAiOjE3MzQwNzI2MTB9.Cyd6ujpcIBHibkdfBBq-OApOHykmVdlzRnRfyp5rfXI')
-  sessionStorage.setItem('userId', '3')
   navigate('/callback?id=3');
 ***REMOVED***;
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,6 +1,12 @@
+import ***REMOVED*** useEffect ***REMOVED*** from "react";
 import ***REMOVED*** googleLogin, naverLogin ***REMOVED*** from "../api/authApi";
 
 const Login = () => ***REMOVED***
+  useEffect(() => ***REMOVED***
+    sessionStorage.setItem('token', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywidXNlck5hbWUiOiLstZzsirntmLgiLCJjb3VwbGVDb2RlIjoiamM3VllhIiwiaWF0IjoxNzMxNDgwNjEwLCJleHAiOjE3MzQwNzI2MTB9.Cyd6ujpcIBHibkdfBBq-OApOHykmVdlzRnRfyp5rfXI')
+    sessionStorage.setItem('userId', '3')
+  ***REMOVED***, [])
+  
   return (
     <div className="flex flex-col justify-center items-center h-full">
       <img className="w-[120px] mb-10" src="logos/Logo.png" alt="" />
