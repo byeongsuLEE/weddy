c437a2e53c3d0b9520266ec88b2399f3d34758ee
이호영 <hoo4053@naver.com>
2024-11-18 11:45:02 +0900

    merge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -54,6 +54,7 @@ function AppContent() ***REMOVED***
       alert("메시지 리스너 실행");
       try ***REMOVED***
         await onMessageListener();
+        alert("메시지 리스너 실행 완료");
       ***REMOVED*** catch ***REMOVED***
         alert("메시지 리스너 오류");
         // 포그라운드 메시지 리스너 오류 처리 로직
