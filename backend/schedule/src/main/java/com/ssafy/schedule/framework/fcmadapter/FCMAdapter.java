cb55ebc8c5d14c211137ed1832a8c0e3ca87574d
이병수 <75319125+byeongsuLEE@users.noreply.github.com>
2024-11-18 17:43:54 +0900

    fix(일정) : fcm 알림 이전 형태로 메시지 전송

--- backend/schedule/src/main/java/com/ssafy/schedule/framework/fcmadapter/FCMAdapter.java
+++ backend/schedule/src/main/java/com/ssafy/schedule/framework/fcmadapter/FCMAdapter.java
@@ -20,15 +20,15 @@ public class FCMAdapter implements FCMOutputPort ***REMOVED***
     public void send(String token, String title, String body) ***REMOVED***
         try ***REMOVED***
             // 알림 메시지 생성
-            Notification notification = Notification.builder()
-                    .setTitle(title)
-                    .setBody(body)
-                    .build();
-
-            Message message = Message.builder()
-                    .setToken(token)
-                    .setNotification(notification)
-                    .build();
+//            Notification notification = Notification.builder()
+//                    .setTitle(title)
+//                    .setBody(body)
+//                    .build();
+//
+//            Message message = Message.builder()
+//                    .setToken(token)
+//                    .setNotification(notification)
+//                    .build();
 
             // 알림 메시지 생성
 //            Notification notification = Notification.builder()
@@ -41,6 +41,12 @@ public class FCMAdapter implements FCMOutputPort ***REMOVED***
 //                    .putData("title", title)
 //                    .putData("body", body)
 //                    .build();
+
+            Message message = Message.builder()
+                    .setToken(token)
+                    .putData("title", title)
+                    .putData("body", body)
+                    .build();
 
             // FCM으로 메시지 전송
             String response = FirebaseMessaging.getInstance().send(message);
